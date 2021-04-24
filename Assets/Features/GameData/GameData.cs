1af02157d8b7c8b1111d8abf6c9fad4e01e9a02e
Daniel <dyrda@in.tum.de>
2021-04-24 17:33:55 +0200

    Impelmented undead and orc versions

--- Assets/Features/GameData/GameData.cs
+++ Assets/Features/GameData/GameData.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using DyrdaDev.Singleton;
 using UniRx;
+using Random = System.Random;
 
 public class GameData : SingletonMonoBehaviour<GameData>
 {
@@ -15,7 +16,12 @@ public class GameData : SingletonMonoBehaviour<GameData>
     public ReactiveProperty<int> score = new ReactiveProperty<int>(0);
    
     [HideInInspector] public LevelTheme currentLevelTheme;
-    
+    private Random LevelThemeRandom = new Random();
+
+    public void Awake()
+    {
+        currentLevelTheme = GetRandomLevelTheme();
+    }
 
     public void IncreaseScore(int value)
     {
@@ -30,10 +36,15 @@ public class GameData : SingletonMonoBehaviour<GameData>
 
     private void GetNextLevelTheme()
     {
-        //Array values = Enum.GetValues(typeof(LevelTheme));
-        //Random random = new Random();
-        //currentLevelTheme = (LevelTheme)values.GetValue(random.Next(values.Length));
-        currentLevelTheme = LevelTheme.Demons; // Currently only demons is implemented.
+        var previousLevelTheme = currentLevelTheme;
+        var newLevelTheme = currentLevelTheme;
+        
+        while (previousLevelTheme == newLevelTheme)
+        {
+            newLevelTheme = GetRandomLevelTheme();
+        }
+
+        currentLevelTheme = newLevelTheme;
     }
 
 
@@ -41,5 +52,11 @@ public class GameData : SingletonMonoBehaviour<GameData>
     {
         ResetScore();
         GetNextLevelTheme();
+    }
+
+    private LevelTheme GetRandomLevelTheme()
+    {
+        Array values = Enum.GetValues(typeof(LevelTheme));
+        return (LevelTheme)values.GetValue(LevelThemeRandom.Next(values.Length));
     }
 }
