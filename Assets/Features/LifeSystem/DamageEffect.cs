8a50020ccf33f11b9f4c6ed6aab34f20d195539c
Xientra <38855570+Xientra@users.noreply.github.com>
2021-10-18 15:06:33 +0200

    Added a explosion effect when an enemy or the background is hit/clicked
    Mostly changed DamageEffect.cs and added a reference (to the exposion) that has to be set in the inspector.
    Therefore the Hitbox prefab is also modified.
    On the Level Scene I only added the "Background Collider" under ENVIRONMENT to detect clicks that are not on enemies.

--- Assets/Features/LifeSystem/DamageEffect.cs
+++ Assets/Features/LifeSystem/DamageEffect.cs
@@ -8,9 +8,13 @@ public class DamageEffect : MonoBehaviour
     private Life _life;
     private Sensor damageCause;
     public float damage = 1.0f;
+    [Space(5)]
+    public GameObject damageVFX;
+    private Camera _cam;
 
     private void Start()
     {
+        _cam = Camera.main;
         _life = GetComponentInParent<Life>();
 
         damageCause = GetComponent<Sensor>();
@@ -20,5 +24,8 @@ public class DamageEffect : MonoBehaviour
     public void DamageCauseSignalDetected(EventArgs args)
     {
         _life.DealDamage(damage);
+
+        Vector3 pos = (args is PositionEventArgs) ? _cam.ScreenToWorldPoint(((PositionEventArgs)args).screenPosition) : transform.position;
+        Instantiate(damageVFX, new Vector3(pos.x, pos.y, damageVFX.transform.position.z), Quaternion.identity);
     }
 }
--- Assets/Features/Sensor/Sensor.cs
+++ Assets/Features/Sensor/Sensor.cs
@@ -1,8 +1,19 @@
 ﻿using System;
 using UnityEngine;
 using UniRx;
+using UnityEngine.EventSystems;
 
 public abstract class Sensor : MonoBehaviour
 {
     public IObservable<EventArgs> SensorTriggered;
+}
+
+public class PositionEventArgs : EventArgs
+{
+    public readonly Vector2 screenPosition;
+
+    public PositionEventArgs(Vector2 pos) : base()
+    {
+        screenPosition = pos;
+    }
 }
