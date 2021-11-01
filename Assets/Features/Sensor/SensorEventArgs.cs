5e59626940dd1c368a8517ede042fb1cf345fc27
Daniel <dyrda@in.tum.de>
2021-11-01 11:10:32 +0100

    Refactored sensors

---
+++ Assets/Features/Sensor/SensorEventArgs.cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UniRx;
+using UnityEngine.EventSystems;
+
+public class SensorEventArgs : EventArgs
+{
+    public readonly PointerEventData associatedPointerPayload;
+
+    public SensorEventArgs(PointerEventData pointerPayload) : base()
+    {
+        associatedPointerPayload = pointerPayload;
+    }
+}
